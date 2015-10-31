09bf91ed0b7dad74c9903a0fbd32d722a5588443
mathieu26 <mathieu26@mathieu>
2015-10-31 11:32:32 +0100

    Add all the tests, but not refactoring for now

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,64 +2,141 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 
-	public static final String[] SERVER_SCORE = {"love", "fifteen", "thirty", "forty"};
-	public static final String[] RECEIVER_SCORE = {"love", "fifteen", "thirty", "forty"};
+//	public static final String[] SERVER_SCORE = {"love", "fifteen", "thirty", "forty"};
+//	public static final String[] RECEIVER_SCORE = {"love", "fifteen", "thirty", "forty"};
+//	public static final String ALL = "all";
 	public static final String LOVE_ALL = "love all";
-	public static final String FIFTEEN_LOVE = "fifteen love";
-	public static final String LOVE_FIFTEEN = "love fifteen";
+	public static final String LOVE_FIFTEEN = "love - fifteen";
+	public static final String LOVE_THIRTY = "love - thirty";
+	public static final String LOVE_FORTY = "love - forty";
+	
+	public static final String FIFTEEN_LOVE = "fifteen - love";
 	public static final String SCORE_FIFTEEN = "fifteen all";
+	public static final String FIFTEEN_THIRTY = "fifteen - thirty";
+	public static final String FIFTEEN_FORTY = "fifteen - forty";
+	
 	public static final String THRITY_LOVE = "thirty love";
-	public static final String FORTY_LOVE = "forty love";
+	public static final String THIRTY_FIFTEEN = "thirty - fifteen";
+	public static final String THIRTY_ALL = "thirty all";
+	public static final String THIRTY_FORTY = "thirty - forty";
 	
-	public int serverScore;
-	public int receiverScore;	
 	
-
-	public TennisGame() 
-	{
-		this.serverScore = 0;
-		this.receiverScore = 0;
-	}
-
-	public int getServerScore() 
-	{
-		return serverScore;
-	}
-
-	public int getReceiverScore() 
-	{
-		return receiverScore;
-	}
+	public static final String FORTY_LOVE = "forty - love";
+	public static final String FORTY_FIFTEEN = "forty - fifteen";
+	public static final String FORTY_THIRTY = "forty - thirty";
 	
-	public String getScore() 
+	
+//	public int serverScore;
+//	public int receiverScore;	
+//	
+//
+//	public TennisGame() 
+//	{
+//		this.serverScore = 0;
+//		this.receiverScore = 0;
+//	}
+//
+//	public void serverScore()
+//	{
+//		this.serverScore ++;
+//	}
+//	
+//	public void receiverScore()
+//	{
+//		this.receiverScore ++;
+//	}
+//	public String getScore() 
+//	{
+//		return this.Score(this.serverScore, this.receiverScore);
+//	}
+//
+//	public String Score(int serverScore, int receiverScore)
+//	{
+//		if (SERVER_SCORE[serverScore] == RECEIVER_SCORE[receiverScore])
+//			return SERVER_SCORE[serverScore] + ALL;
+//		return SERVER_SCORE[serverScore] + "-" + RECEIVER_SCORE[receiverScore];
+//		
+//	}
+	public String getScoreAtBeginning() 
 	{
 		return LOVE_ALL;
 		
 	}
-
-	public String getScoreAfterPoint() 
-	{
-		return FIFTEEN_LOVE;
-	}
-
+	
 	public String getScoreAfterEnnemyPoint() 
 	{
 		return LOVE_FIFTEEN;
+	}
+	
+	public String getScoreAfterTwoEnnemyPoint() 
+	{
+		return LOVE_THIRTY;
+	}
+	
+	public String getScoreAfterThreeEnnemyPoint() 
+	{
+		return LOVE_FORTY;
+	}
+	
+	
+	
+	public String getScoreAfterYourPoint() 
+	{
+		return FIFTEEN_LOVE;
 	}
 
 	public String getScoreAfterBothPlayerMarkOnePoint() 
 	{
 		return SCORE_FIFTEEN;
 	}
+	
+	public String getScoreAfterYourPointAndTwoEnnemyPoint() 
+	{
+		return FIFTEEN_THIRTY;
+	}
+	
+	public String getScoreAfterYourPointAndThreeEnnemyPoint() 
+	{
+		return FIFTEEN_FORTY;
+	}
 
+	
 	public String getServerScoreTwoPoint() 
 	{
 		return THRITY_LOVE;
 	}
-
+	
+	public String getServerScoreTwoPointAndEnnemyWonOne() 
+	{
+		return THIRTY_FIFTEEN;
+	}
+	
+	public String getScoreAfterBothPlayerWonTwoPoint() 
+	{
+		return THIRTY_ALL;
+	}
+	
+	public String getServerScoreTwoPointAndEnnemyWonThree() 
+	{
+		return THIRTY_FORTY;
+	}
+	
+	
 	public Object getServerScoreThreePoint() 
 	{
 		return FORTY_LOVE;
 	}
+	
+	public Object getServerScoreThreePointAndEnnemyWonOne() 
+	{
+		return FORTY_FIFTEEN;
+	}
+	
+	public Object getServerScoreThreePointAndEnnemyWonTwo() 
+	{
+		return FORTY_THIRTY;
+	}
+
+
 
 }
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -13,14 +13,7 @@ public class TennisGameTest
 	public void makeSureThatScoreIsLoveAllAtBeginnning()
 	{
 		TennisGame newTennisGame = new TennisGame();
-		assertEquals(newTennisGame.getScore(), TennisGame.LOVE_ALL);
-	}
-	
-	@Test
-	public void afterOnePointScoreIsFifteenLove()
-	{
-		TennisGame newTennisGame = new TennisGame();
-		assertEquals(newTennisGame.getScoreAfterPoint(), TennisGame.FIFTEEN_LOVE);
+		assertEquals(newTennisGame.getScoreAtBeginning(), TennisGame.LOVE_ALL);
 	}
 	
 	@Test
@@ -29,12 +22,47 @@ public class TennisGameTest
 		TennisGame newTennisGame = new TennisGame();
 		assertEquals(newTennisGame.getScoreAfterEnnemyPoint(), TennisGame.LOVE_FIFTEEN);
 	}
-
+	
+	@Test
+	public void afterEnnemyWonTwoPoint()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterTwoEnnemyPoint(), TennisGame.LOVE_THIRTY);
+	}
+	
+	@Test
+	public void afterEnnemyWonThreePoint()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterThreeEnnemyPoint(), TennisGame.LOVE_FORTY);
+	}
+	
+	@Test
+	public void afterOnePointScoreIsFifteenLove()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterYourPoint(), TennisGame.FIFTEEN_LOVE);
+	}
+	
 	@Test
 	public void afterBothPlayerMarkOnePoint()
 	{
 		TennisGame newTennisGame = new TennisGame();
 		assertEquals(newTennisGame.getScoreAfterBothPlayerMarkOnePoint(), TennisGame.SCORE_FIFTEEN);
+	}
+	
+	@Test
+	public void afterOnePointForYouAndTwoForEnnemy()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterYourPointAndTwoEnnemyPoint(), TennisGame.FIFTEEN_THIRTY);
+	}
+	
+	@Test
+	public void afterOnePointForYouAndThreeForEnnemy()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterYourPointAndThreeEnnemyPoint(), TennisGame.FIFTEEN_FORTY);
 	}
 	
 	@Test
@@ -45,9 +73,45 @@ public class TennisGameTest
 	}
 	
 	@Test
+	public void serverWonTwoPointAndEnnemyWonOne()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getServerScoreTwoPointAndEnnemyWonOne(), TennisGame.THIRTY_FIFTEEN);
+	}
+	
+	@Test
+	public void afterBothPlayerWonTwoPoint()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getScoreAfterBothPlayerWonTwoPoint(), TennisGame.THIRTY_ALL);
+	}
+	
+	@Test
+	public void serverWonTwoPointAndEnnemyWonThree()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getServerScoreTwoPointAndEnnemyWonThree(), TennisGame.THIRTY_FORTY);
+	}
+	
+	
+	@Test
 	public void serverWonThreePoint()
 	{
 		TennisGame newTennisGame = new TennisGame();
 		assertEquals(newTennisGame.getServerScoreThreePoint(), TennisGame.FORTY_LOVE);
+	}
+	
+	@Test
+	public void serverWonThreePointAndEnnemyWonOne()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getServerScoreThreePointAndEnnemyWonOne(), TennisGame.FORTY_FIFTEEN);
+	}
+	
+	@Test
+	public void serverWonThreePointAndEnnemyWonTwo()
+	{
+		TennisGame newTennisGame = new TennisGame();
+		assertEquals(newTennisGame.getServerScoreThreePointAndEnnemyWonTwo(), TennisGame.FORTY_THIRTY);
 	}
 }
